959198a45e1be2d0bd6b587cee842b6ece5f014e
SatanistiVuohi <kumpulatommi@gmail.com>
2024-09-12 11:06:27 +0300

    Login state control

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-
+import { useState } from "react"
 
 function App() {
-  
+
+  const [login, setLogin] = useState(false);
+
+  function loginPressed() {
+    setLogin(true);
+  }
+
+
+
 
   return (
-   <div>
-    
-   </div>
-  )
+    <div>
+      <button onClick={loginPressed}>Login</button>
+    </div>
+  );
 }
 
 export default App
